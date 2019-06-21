a3cfeccf54aa39a593a77c2ff652ef869d71bc3e
Jaishree.Bawaskar <Jaishree.Bawaskar@e-Zest.in>
2019-06-21 18:28:36 +0530

    add toGrams in Miligrams

---
+++ Miligrams.java
@@ -0,0 +1,10 @@
+public class Miligrams implements IUnit
+{ 
+    public double toGrams(double miligrams)
+    {
+        double target;
+        target = s / 1000 ;
+        return targrt;
+    }
+
+}
