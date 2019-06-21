a3409b70fb94b44512c05426f5d2254a82fbe05d
Jaishree.Bawaskar <Jaishree.Bawaskar@e-Zest.in>
2019-06-21 18:34:49 +0530

    renamed variables in toGrams

--- Miligrams.java
+++ Miligrams.java
@@ -2,9 +2,9 @@ public class Miligrams implements IUnit
 { 
     public double toGrams(double miligrams)
     {
-        double target;
-        target = s / 1000 ;
-        return targrt;
+        double grams;
+        grams = miligrams / 1000 ;
+        return miligrams;
     }
 
 }
